154c36b44762f8a22532a0084634a8b9d48bfc56
Isaac MENARD <35576419+isaacmenard@users.noreply.github.com>
2022-05-11 11:38:06 +0200

    gamemanager

---
+++ GameGameJam/Assets/ScriptableObjects/GameManagerSciptableObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameManager", menuName = "GameManagerObject", order = 0)]
+public class GameManagerSciptableObject : ScriptableObject
+{
+    [Header("Party")]
+    public float scoreMultiplicateur;//fait
+    public float coinDivideurByScore;//fait
+    public float capsulePvPuissance;//fait
+    public float capsulePvCoolDownMin;//fait
+    public float capsulePvCoolDownMax;//fait
+
+    [Space, Header("Planet")]
+    public float MinPVPlanetToUseProtection;//fait
+    public float scalePlanetOffset;//fait
+    public Vector3 rotationPlanet;//fait
+    public float puissanceLerpScale;//fait
+
+    [Space, Header("Player")]
+    public float speedPlayer;//fait
+    public float puissanceDash;
+    public float cooldownDash;
+    public float cooldownDashEnd;
+
+    [Space, Header("Protection")]
+    public float cooldownMaxProtection;
+    public float protectionQuantity;//fait
+}
